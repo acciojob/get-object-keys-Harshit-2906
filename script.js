02c5e2356140081172937ac5cb4145ff57a70e6b
Harshit-2906 <125068698+Harshit-2906@users.noreply.github.com>
2024-07-13 19:03:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+// Create the student object with a name property
+let student = {
+    name: "John Doe"
+};
+
+// Add a getKeys() method to the Object prototype
+Object.prototype.getKeys = function() {
+    return Object.keys(this);
+};
+
+// Test the getKeys method on the student object
+console.log(student.getKeys());
